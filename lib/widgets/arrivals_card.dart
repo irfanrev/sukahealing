a09ac833b59e669d74c2c45f3d66f8faf454339b
Irfan Maulana <irfan.resimen2018@gmail.com>
2022-06-22 19:28:04 +0700

    Home new arrivals

--- lib/widgets/arrivals_card.dart
+++ lib/widgets/arrivals_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:suka_healing/theme.dart';
 
 class ArrivalCard extends StatelessWidget {
   const ArrivalCard({Key? key}) : super(key: key);
@@ -6,7 +8,9 @@ class ArrivalCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(top: 12),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
             height: 100,
@@ -14,8 +18,50 @@ class ArrivalCard extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               color: Colors.white,
+              image: const DecorationImage(
+                image: AssetImage('assets/img/curug.png'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
+          const SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Curug',
+                  style: GoogleFonts.poppins(
+                    color: secondaryText,
+                    fontSize: 13,
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'Curug Cimarinjung',
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: primaryText,
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'Ciemas, Sukabumi',
+                  style: GoogleFonts.poppins(
+                    color: secondaryColor,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
